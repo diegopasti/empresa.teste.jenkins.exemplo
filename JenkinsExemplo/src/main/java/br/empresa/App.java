6d308e239e9e8dbf84d22ffaf3884e311bd3d40d
Diego Pasti <diegopasti@gmail.com>
2014-10-08 18:56:19 -0300

    teste com o bitbucket

--- JenkinsExemplo/src/main/java/br/empresa/App.java
+++ JenkinsExemplo/src/main/java/br/empresa/App.java
@@ -5,6 +5,6 @@ import br.empresa.calculadora.Calculadora;
 public class App {
     public static void main( String[] args ){
     	Calculadora calc = new Calculadora();
-    	System.out.println("Mostrando a integração continua pro Breno");
+    	System.out.println("Testando o build automatico pelo bitBucket");
     }
 }
