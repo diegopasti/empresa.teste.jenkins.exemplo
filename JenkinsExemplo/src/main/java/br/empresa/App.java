bd91638d0131f2645ebf5f17d1a883cf27f90757
Diego Pasti <diegopasti@gmail.com>
2014-12-16 14:51:27 -0200

    Verificando se o numero da versão vai mudar sozinho

--- JenkinsExemplo/src/main/java/br/empresa/App.java
+++ JenkinsExemplo/src/main/java/br/empresa/App.java
@@ -5,6 +5,14 @@ import br.empresa.calculadora.Calculadora;
 public class App {
     public static void main( String[] args ){
     	Calculadora calc = new Calculadora();
-    	System.out.println("Testando o build automatico pelo bitBucket");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	System.out.println("Sera que gera numero de versao automatico?");
+    	
     }
 }
