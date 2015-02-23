9f5a19006784525cbd16152e9ccf96677bb9052f
Diego Pasti <diegopasti@gmail.com>
2015-02-23 18:33:48 -0300

    Teste de repetição

--- JenkinsExemplo/src/main/java/br/empresa/App.java
+++ JenkinsExemplo/src/main/java/br/empresa/App.java
@@ -6,5 +6,11 @@ public class App {
     public static void main( String[] args ){
     	Calculadora calc = new Calculadora();
     	System.out.println("Testando"+calc.toString());
+    	System.out.println("Testando"+calc.toString());
+    	System.out.println("Testando"+calc.toString());
+    	System.out.println("Testando"+calc.toString());
+    	System.out.println("Testando"+calc.toString());
+    	System.out.println("Testando"+calc.toString());
+    	System.out.println("Testando"+calc.toString());
     }
 }
